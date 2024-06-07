102183f56efd3502a3985310c5b1fec7bbb0760b
Soham2395 <soham2395.be22@chitkara.edu.in>
2024-06-07 17:03:04 +0530

    Scroller location changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -90,7 +90,7 @@ const Hero = () => {
 
           <div className="flex">
             <a
-              href="#tech"
+              href="#contact"
               className="  w-fit mx-auto flex items-center justify-center gap-6 bg-tertiary lg:px-7 lg:py-3 px-4 py-2 rounded-xl lg:mt-10 mt-5 cursor-pointer max-[350px]:hidden select-none"
             >
               <div className=" group border-[3px] border-white rounded-full md:h-12 h-10 w-7 flex justify-center lg:scale-100 scale-75">
